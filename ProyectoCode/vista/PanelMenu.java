f618c9396963b54956d476556c1ded49fb5c97f3
angerson25 <angerson2224639@correo.uis.edu.co>
2023-06-09 08:10:31 -0500

    word y botones invisibles

--- ProyectoCode/vista/PanelMenu.java
+++ ProyectoCode/vista/PanelMenu.java
@@ -29,69 +29,84 @@ public class PanelMenu extends JDialog
         //creacion de los labels
         lbActividades= new JLabel("ACTIVIDADES",JLabel.CENTER);
         lbActividades.setFont(new Font("Gill Sans MT", Font.BOLD, 35));
-        lbActividades.setBounds(300,20,300,40);
+        lbActividades.setBounds(300,90,300,40);
         Color blanco =new Color(255,255,255);
         lbActividades.setForeground(blanco);
         add(lbActividades);
 
-        //creacion boton jugar1
-        btJugar1 = new JButton("JUGAR");
-        btJugar1.setFont(new Font("Gill Sans MT", Font.BOLD, 13));
-        btJugar1.setBounds(50,430,170,50); 
-        btJugar1.setActionCommand("jugar1");
-        btJugar1.setBackground(blanco);
-        this.add(btJugar1);
 
         //añadir imagen ahorcado
         ahorcado = new ImageIcon(getClass().getResource("/vista/img/ahorcado.png"));
         lbAhorcado = new JLabel(ahorcado);
-        lbAhorcado.setBounds(50,150,170,170);
+        lbAhorcado.setBounds(50,220,170,170);
         add(lbAhorcado);
 
+        //creacion boton jugar1
+        btJugar1 = new JButton("");
+        btJugar1.setFont(new Font("Gill Sans MT", Font.BOLD, 13));
+        btJugar1.setBounds(50,220,170,170);
+        btJugar1.setActionCommand("jugar1");
+        btJugar1.setOpaque(false);
+        btJugar1.setContentAreaFilled(false);
+        btJugar1.setBorderPainted(false);
+        this.add(btJugar1);
 
 
-
-        //creacion boton jugar2
-        btJugar2 = new JButton("JUGAR");
-        btJugar2.setFont(new Font("Gill Sans MT", Font.BOLD, 13));
-        btJugar2.setBounds(250,430,170,50); 
-        btJugar2.setActionCommand("jugar2");
-        btJugar2.setBackground(blanco);
-        this.add(btJugar2);
 
         //añadir imagen segundo juego
         iArreglar = new ImageIcon(getClass().getResource("/vista/img/arreglar.png"));
         lbArreglar = new JLabel(iArreglar);
-        lbArreglar.setBounds(250,150,170,170);
+        lbArreglar.setBounds(250,220,170,170);
         add(lbArreglar);
 
-        //creacion boton jugar3
-        btJugar3 = new JButton("JUGAR");
-        btJugar3.setFont(new Font("Gill Sans MT", Font.BOLD, 13));
-        btJugar3.setBounds(450,430,170,50);
-        btJugar3.setActionCommand("jugar3");
-        btJugar3.setBackground(blanco);
-        this.add(btJugar3);
+
+        //creacion boton jugar2
+        btJugar2 = new JButton("");
+        btJugar2.setFont(new Font("Gill Sans MT", Font.BOLD, 13));
+        btJugar2.setBounds(250,220,170,170);
+        btJugar2.setActionCommand("jugar2");
+        btJugar2.setOpaque(false);
+        btJugar2.setContentAreaFilled(false);
+        btJugar2.setBorderPainted(false);
+        this.add(btJugar2);
+
+
 
         //añadir imagen caracteres
         iCaracteres = new ImageIcon(getClass().getResource("/vista/img/caracteres.png"));
         lbCaracteres = new JLabel(iCaracteres);
-        lbCaracteres.setBounds(450,150,170,170);
+        lbCaracteres.setBounds(450,220,170,170);
         add(lbCaracteres);
 
-        //creacion boton jugar4
-        btJugar4 = new JButton("JUGAR");
-        btJugar4.setFont(new Font("Gill Sans MT", Font.BOLD, 13));
-        btJugar4.setBounds(650,430,170,50);
-        btJugar4.setActionCommand("jugar4");
-        btJugar4.setBackground(blanco);
-        this.add(btJugar4);
+        //creacion boton jugar3
+        btJugar3 = new JButton("");
+        btJugar3.setFont(new Font("Gill Sans MT", Font.BOLD, 13));
+        btJugar3.setBounds(450,220,170,170);
+        btJugar3.setActionCommand("jugar3");
+        btJugar3.setOpaque(false);
+        btJugar3.setContentAreaFilled(false);
+        btJugar3.setBorderPainted(false);
+        this.add(btJugar3);
+
+
 
         //añadir imagen identificar
         iIdentificar = new ImageIcon(getClass().getResource("/vista/img/identificar.png"));
         lbIdentificar = new JLabel(iIdentificar);
-        lbIdentificar.setBounds(650,150,170,170);
+        lbIdentificar.setBounds(650,220,170,170);
         add(lbIdentificar);
+
+        //creacion boton jugar4
+        btJugar4 = new JButton("");
+        btJugar4.setFont(new Font("Gill Sans MT", Font.BOLD, 13));
+        btJugar4.setBounds(650,220,170,170);
+        btJugar4.setActionCommand("jugar4");
+        btJugar4.setOpaque(false);
+        btJugar4.setContentAreaFilled(false);
+        btJugar4.setBorderPainted(false);
+        this.add(btJugar4);
+
+
 
 
 
